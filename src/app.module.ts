2b744363cc3894f9f139546da2c8e585c9518d38
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2024-06-08 22:37:25 +0300

    modules init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HistoryModule } from './history/history.module';
 import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [TaskModule],
+  imports: [TaskModule, HistoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
